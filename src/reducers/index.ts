9610085565092fef75718213fd6e8f865fafe1ac
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-10-13 16:22:40 +0100

    Validating Store Structure

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { Todo } from '../actions';
+
+export interface StoreState {
+  todos: Todo[];
+}
 
 
-export const reducers = combineReducers({
+export const reducers = combineReducers<StoreState>({
   //silly dummy reducer - if we dont have one redux will throw an error
-  counter: () => 1
+  //counter: () => 1
+  todos: todosReducer
 });
