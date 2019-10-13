5b3b2c3741fce6809de56b974cf9588839e28ff2
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-10-13 17:00:43 +0100

    Breaking Out Action Creators

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -13,10 +13,14 @@ export interface FetchTodosAction {
   payload: Todo[];
 }
 
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
-
   return async (dispatch: Dispatch) => {
     //inside of response.data we will have an array of objects that satisfies the Todo interface
     const response = await axios.get<Todo[]>(url);
@@ -26,4 +30,11 @@ export const fetchTodos = () => {
       payload: response.data
     });
   };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id
+  }
 };
